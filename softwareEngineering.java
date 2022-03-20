ae1402aed2d5eb126d497be84c95e638e0b145b0
haneenmustafa03 <haneenmustafa500@gmail.com>
2022-03-19 21:56:16 -0400

    first commit

---
+++ softwareEngineering.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class softwareEngineering {
+    public static void main(String [] Args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input first number");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Input second number");
+        int num2 = scanner.nextInt();
+
+        System.out.println(num1 + num2);
+
+    }
+}
