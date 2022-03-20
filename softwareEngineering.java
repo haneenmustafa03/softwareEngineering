661ed34c75e219556722d1bc821913f62a6bbc3c
haneenmustafa03 <haneenmustafa500@gmail.com>
2022-03-19 22:04:23 -0400

    subtracting two numbers

--- softwareEngineering.java
+++ softwareEngineering.java
@@ -3,13 +3,31 @@ public class softwareEngineering {
     public static void main(String [] Args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Input first number");
-        int num1 = scanner.nextInt();
+        System.out.println("What would you like to do?");
+        System.out.println("1. Add");
+        System.out.println("2. Subtract");
+        int menuOption = scanner.nextInt();
 
-        System.out.println("Input second number");
-        int num2 = scanner.nextInt();
+        switch(menuOption) {
+            case 1:
+                System.out.println("Input first number");
+                int num1 = scanner.nextInt();
 
-        System.out.println(num1 + num2);
+                System.out.println("Input second number");
+                int num2 = scanner.nextInt();
+
+                System.out.println(num1 + num2);
+            break;
+
+            case 2:
+                System.out.println("Input first number");
+                num1 = scanner.nextInt();
+
+                System.out.println("Input second number");
+                num2 = scanner.nextInt();
+
+                System.out.println(num1 - num2);
+        }
 
     }
 }
